07c427c67bc53d9131d184b5dc35cc3373198ee4
Simplex <kontakt.simplex@outlook.de>
2022-02-03 18:04:33 +0100

    Create bot.py

---
+++ source/Bot/bot.py
@@ -0,0 +1,27 @@
+import nextcord
+from nextcord.ext import commands
+import os
+from loguru import logger
+
+from .init import __init__
+
+def start(version):
+    bot = commands.Bot(command_prefix="?", case_insensitive=True)
+
+    @bot.event
+    async def on_ready():
+        logger.info(f"Eingeloggt als {bot.user} | {version}")
+        await bot.change_presence(
+        activity=nextcord.Activity(
+            type=nextcord.ActivityType.watching,
+            name=f"?help | {version}"),
+            status=nextcord.Status.online)
+
+    @bot.command(name="botinfo")
+    async def botinfo(ctx):
+        embed = nextcord.Embed(title="Bot-Informationen", description=f"Version: {version}", colour=nextcord.Colour.gold(), timestamp=ctx.message.created_at)
+        return await  ctx.send(embed=embed)
+
+    __init__(bot, version)
+
+    bot.run(os.getenv("tokendev"))
