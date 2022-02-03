7525228b4557c51ff45a03b59eff2975b239bb02
Simplex <kontakt.simplex@outlook.de>
2022-02-04 00:24:54 +0100

    fix: :ambulance: fix dev.py not working correctly
    dev.py had multiple errors, that stopped us from reloading cogs without restarting the bot

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 from loguru import logger
 from source.Base.Bot.bot import start
 
-__version__ = "1.1.1+2022.02" # <MAJOR>.<MINOR>.<PATCH>-<IDENTIFIER>.<IDENTIFIER>+<BUILDMETA>
+__version__ = "1.1.3+2022.02" # <MAJOR>.<MINOR>.<PATCH>-<IDENTIFIER>.<IDENTIFIER>+<BUILDMETA>
 exts        = [
             "developer"
             ]
