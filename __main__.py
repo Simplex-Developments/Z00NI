f737dee09b54b38eb075f857f67a68a8b6c95342
Simplex <kontakt.simplex@outlook.de>
2022-02-04 00:15:44 +0100

    refactor: :art: move exts = [] to __main__.py

--- __main__.py
+++ __main__.py
@@ -2,5 +2,9 @@ from loguru import logger
 from source.Base.Bot.bot import start
 
 __version__ = "1.0.0+2022.02" # <MAJOR>.<MINOR>.<PATCH>-<IDENTIFIER>.<IDENTIFIER>+<BUILDMETA>
+exts        = [
+            
+            ]
+
 
 start(__version__)
--- source/Base/Bot/init.py
+++ source/Base/Bot/init.py
@@ -1,6 +1,5 @@
 from loguru import logger
 from ..Helper.extloader import extloader
 
-def __init__(bot, version):
-    exts = []
+def __init__(bot, exts):
     extloader(bot, exts)
