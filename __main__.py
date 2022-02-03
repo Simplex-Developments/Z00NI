f2b708d28c6ddc3749db51ed6563fb6921964b0a
Simplex <kontakt.simplex@outlook.de>
2022-02-03 22:09:01 +0100

    style: :art: change version format

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 from loguru import logger
 from source.Bot.bot import start
 
-__version__ = "0.2.0-beta.1+2022.02" # <MAJOR>.<MINOR>.<PATCH>-<IDENTIFIER>.<IDENTIFIER>+<BUILDMETA>
+__version__ = "0.2.0-b.1+2022.02" # <MAJOR>.<MINOR>.<PATCH>-<IDENTIFIER>.<IDENTIFIER>+<BUILDMETA>
 
 start(__version__)
